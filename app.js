b11bcf645788915543d0db16516ec56fe8df1263
AhmedDhifallah <ahmed.dhifallah@esprit.tn>
2023-03-15 15:20:09 +0100

    cors & port 3001

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const cors = require('cors')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -12,7 +12,7 @@ var config = require('./database/mongodb');
 mongoose.connect(config.mongo.uri);
 
 var app = express();
-
+app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
