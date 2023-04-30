d2e8b1db1a1bc7f5382f849c16a9f6895c3ef67b
yosri-hk <yosri.hajkhlifaesprit.tn>
2023-04-30 23:46:23 +0100

    chatroom message

--- app.js
+++ app.js
@@ -43,7 +43,6 @@ app.use('/api', paymentRouter);
 app.use('/events',eventRouter);
 app.use('/chat',chatRouter);
 app.use("/message", messageRoutes);
-app.use('/payevent',payEventRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
